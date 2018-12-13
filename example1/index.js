e8f1948420d2c8cdd0a200c93e36730915302398
onlinekini@gmail.com <onlinekini@gmail.com>
2018-12-13 22:31:20 +0530

    first checkin

---
+++ example1/index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter :(x,y) => (2*(x+y)),
+    area : (x,y) => x * y
+};
+
+
+var solveRectangle  = (l,b) => {
+    console.log("Solving for rectanle with l = " + l + " and b = " + b)
+
+    if (l <= 0 || b <= 0) {
+        console.log("Rectangle dimensions should be greater than ZERO l = " + l + " and b = " + b)
+    } else {
+        console.log("Area = " , rect.area(l,b))
+        console.log("Perimeter = " , rect.perimeter(l,b))
+    }
+}
+
+solveRectangle(3,4)
+solveRectangle(0,5)
+solveRectangle(5,-3)
