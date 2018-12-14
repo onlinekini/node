09bf46c4f2bad7ac53a9b1d89fa9064de50ac342
onlinekini@gmail.com <onlinekini@gmail.com>
2018-12-14 17:36:22 +0530

    Files and TODO

--- example1/index.js
+++ example1/index.js
@@ -1,6 +1,6 @@
 var rect = require("./rectangle")
 
-var solveRectangle  = (l,b) => {
+/*var solveRectangle  = (l,b) => {
     console.log("Solving for rectanle with l = " + l + " and b = " + b)
 
     if (l <= 0 || b <= 0) {
@@ -9,7 +9,21 @@ var solveRectangle  = (l,b) => {
         console.log("Area = " , rect.area(l,b))
         console.log("Perimeter = " , rect.perimeter(l,b))
     }
+} */
+
+function solveRectangle (l,b) {
+    console.log("Processing rect with l = " + l + " and b " + b);
+    rect(l, b, (err, rectangle) => {
+        if (err) {
+            console.log("Error" + err.message)
+        } else {
+            console.log("Area : " + rectangle.area())
+            console.log("Perimeter : " + rectangle.perimeter())
+        }
+   }) 
+   console.log("After the function call");
 }
+
 
 solveRectangle(3,4)
 solveRectangle(0,5)
--- example1/rectangle.js
+++ example1/rectangle.js
@@ -1,2 +1,21 @@
-exports.perimeter = (x,y) => 2 * (x + y)
-exports.area = (x,y) => x * y 
+// Without call backs
+//exports.perimeter = (x,y) => 2 * (x + y)
+//exports.area = (x,y) => x * y 
+
+// Using call backs
+// There is delay of 2 seconds for any calls
+module.exports = (x,y, callback) => {
+    if (x <= 0 || y <= 0) {
+        setTimeout(() =>  
+            callback(new Error("Rectangle dimensions should be greater than ZERO l = " + x + " and b = " + y), 
+                     null), 
+            2000)
+    } else {
+        setTimeout(() => 
+            callback(null, {
+                    perimeter: () => 2 * (x + y), // X & Y is fetched from the outer call - CLosure
+                    area: () => x * y 
+                }),
+            2000)
+    }
+}
