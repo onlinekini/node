d8425c360b2d8dc6b2a4365b8e4041b2ad22be4b
onlinekini@gmail.com <onlinekini@gmail.com>
2018-12-17 22:38:24 +0530

    express and express REST

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const http = require('http')
 const morgan = require('morgan') //Morgan logs info to the console
+const bodyParser = require('body-parser')
+
 
 const host = 'localhost'
 const port = 3000
@@ -8,6 +10,7 @@ const port = 3000
 const app = express()
 app.use(morgan('dev')) // dev mode so log more information. 
 app.use(express.static(__dirname + '/public')) //__dirname is "THIS" directory
+//app.use(bodyParser.json())
 
 // serve up the index.html on / as it is the standard
 
