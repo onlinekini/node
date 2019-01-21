e49f913a7ea42683380b288b21885e3998844c05
Vivekanand Kini <vkini@opentext.com>
2019-01-21 10:00:32 +0530

    additional schema

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -10,20 +10,32 @@ connect
         console.log("connected correctly to server")
         /* var newDish = dishes({name: "Paneer pizza", description: "Pizza with paneer topping"})
         newDish.save() */
-        
-        dishes.create({name: "Paneer pizza", description: "Pizza with paneer topping"})
-            
+        dishes.deleteMany({})
+        .then (result => {
+            console.log("DELETED", result)
+            return dishes.create({name: "Paneer pizza", description: "Pizza with paneer topping"})
+        })    
         .then(result => {
             console.log("CREATE -> ", result)
             return dishes.findOne({name: "Paneer pizza"}).exec()
         }) 
-        .then (data => {
-            console.log("FOUND ! : ", data)
-            data.set({description: "Pizza with YUMMY paneer topping"})
-            return data.save()
+        .then (aDish => {
+            console.log("FOUND ! : ", aDish)
+            aDish.set({description: "Pizza with YUMMY paneer topping"})
+            return aDish.save()
+        })
+        .then (aDish => {
+            console.log("UPDATED ! : ", aDish)
+            aDish.comments.push({rating: 1, comment:"Panneer was dry", author:"who else!"})
+            return aDish.save()
+        })
+        .then (aDish => {
+            console.log("COMMENT1 ! : ", aDish)
+            aDish.comments.push({rating: 4, comment:"Panneer was great", author:"me!"})
+            return aDish.save()
         }) 
         .then (data => {
-            console.log("UPDATED ! : ", data)
+            console.log("COMMENT 2 ! : ", data)
             return dishes.deleteMany({})
         }) 
         .then (result => {
--- node-mongoose/models/dishes.js
+++ node-mongoose/models/dishes.js
@@ -3,6 +3,23 @@ const mongoose = require ('mongoose')
 
 const Schema = mongoose.Schema
 
+const commentsSchema = new Schema ({
+    rating : {
+        type : Number,
+        min : 1,
+        max : 10,
+        required: true
+    },
+    comment : {
+        type: String,
+        required: true
+    },
+    author : {
+        type : String,
+        required : true
+    }
+}, {timestamps : true})
+
 
 const dishSchema = new Schema({
     name:{
@@ -13,8 +30,12 @@ const dishSchema = new Schema({
     description:{
         type:String,
         required:true
-    }
+    },
+    comments : [commentsSchema]
 },{timestamps : true})
+
+
+
 
 var dishes = mongoose.model('Dish', dishSchema)
 
